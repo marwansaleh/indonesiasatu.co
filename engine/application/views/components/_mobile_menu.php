9fd6bb207d68a73e6def4d01c3dd7a9b5bab7353
marwansaleh <marwansaleh@192.168.1.102>
2015-10-30 00:40:08 +0700

    Update mobile title

--- engine/application/views/components/_mobile_menu.php
+++ engine/application/views/components/_mobile_menu.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="<?php echo site_url('home'); ?>">
-                NASABAH.CO
+                IndonesiaSatu
             </a>
         </div>
         <div class="collapse navbar-collapse" id="nasabah-navbar">
