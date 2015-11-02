797684d14ebf7fdf9dc6dc1694c9b1b58fba6c90
marwansaleh <marwansaleh@192.168.1.12>
2015-11-02 13:43:15 +0700

    Add all category in bottom

--- engine/application/views/components/_body_footer.php
+++ engine/application/views/components/_body_footer.php
@@ -1,50 +1,23 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-sm-4 footer-widget">
-                <h3>Redaksi</h3>
-                <p>
-                    Sewaktu hidup, co-pilot Germanwings, Andreas Lubitz, 
-                    pernah mengatakan bahwa semua orang bakal mengenal 
-                    dan mengenangnya. Hal itu dikatakannya kepada 
-                    kekasihnya, Maria W. 
-                </p>
-                <p>
-                    Perekam suara pada kotak hitam 
-                    menunjukkan bahwa Lubitz (27 tahun) 
-                    sengaja mencegah pilot masuk lagi ke dalam kokpit. 
-                    Kemudian, dia sengaja menubrukkan pesawat ke tebing gunung.
-                </p>
-            </div>
-            <div class="col-sm-4 footer-widget">
-                <h3>PHOTOSTREAM</h3>
-                <?php if (isset($photo_news)):?>
-                <div class="row flickr-gallery">
-                    <?php foreach ($photo_news as $photo):?>
-                    <div class="col-xs-2 col-sm-3">
-                        <a href="<?php echo get_image_thumb($photo->image_url, IMAGE_THUMB_ORI); ?>" rel="prettyPhoto[newsfoto]" title="<?php echo $photo->description; ?>">
-                            <img src="<?php echo get_image_thumb($photo->image_url, IMAGE_THUMB_SQUARE); ?>" alt="<?php echo $photo->title; ?>">
-                        </a>
-                    </div>
+            <div class="col-sm-12"><h3>Semua Kategori</h3></div>
+            <?php for($i=0;$i<4;$i++): ?>
+            <div class="col-sm-3">
+                <div class="list-group">
+                    <?php foreach ($categories_articles[$i] as $catbottom): ?>
+                    <a class="list-group-item" href="<?php echo site_url('category/'.$catbottom->slug); ?>">
+                        <span class="badge"><?php echo $catbottom->article_count; ?></span>
+                        <?php echo $catbottom->name; ?>
+                    </a>
                     <?php endforeach; ?>
                 </div>
-                <?php endif; ?>
             </div>
-            <div class="col-sm-4 footer-widget">
-                <h3>LATEST TWEETS</h3>
-                <div class="tweet first-child">
-                    <span class="time">about an hour ago</span> Want to learn more about
-                    Photoshop Layer Styles? This 12-part series explains
-                    everything you need to know. - <a href="http://ow.ly/dfhsJ">ow.ly/dfhsJ</a>
-                </div>
-                <div class="tweet">
-                    <span class="time">about an hour ago</span> Premium: Create a Flying Pig
-                    with Poser and Photoshop - <a href="http://ow.ly/dfhsJ">ow.ly/dfhsJ</a>
-                </div>
-            </div>
+            <?php endfor; ?>
         </div>
     </div>
 </footer>
+
 <!--
 <div class="sub-footer">
     <div class="container">
