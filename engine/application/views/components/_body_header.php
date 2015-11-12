234f6478773311dd888d960972947a8065bc6557
marwansaleh <marwansaleh@192.168.0.108>
2015-11-12 16:08:15 +0700

    Body header update

--- engine/application/views/components/_body_header.php
+++ engine/application/views/components/_body_header.php
@@ -59,50 +59,6 @@
                 <?php endif; ?>
             </div>
         </div>
-        <!--
-        <div class="row menu-line">
-            <div class="col-sm-6">
-                <nav>
-                    <ul>
-                        <li class="active"><a href="<?php echo site_url(); ?>">Home</a></li>
-                        <li>
-                            <?php if ($is_logged_in): ?>
-                            <a href="<?php echo site_url('auth/logout'); ?>">Logout</a>
-                            <?php else: ?>
-                            <a href="<?php echo site_url('auth'); ?>">Login</a>
-                            <?php endif; ?>
-                        </li>
-                        <li><a href="<?php echo site_url('contact'); ?>">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-            
-            <div class="col-sm-3 social-links">
-                <nav>
-                    <ul>
-                    <li><a href="#" class="facebook">Facebook</a></li>
-                    <li><a href="#" class="twitter">Twitter</a></li>
-                    <li><a href="#" class="pinterest">Pinterest</a></li>
-                    <li><a href="#" class="googleplus">Google+</a></li>
-                </ul>
-                </nav>
-            </div>
-            <div class="col-sm-3">
-                <div class="row">
-                    <div class="pull-right">
-                        <form method="post" action="<?php echo site_url('search'); ?>">
-                            <div class="input-group input-group-sm">
-                                <input class="form-control" type="text" name="search" placeholder="Search...">
-                                <div class="input-group-btn">
-                                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-        
         
         <div class="row main-nav">
             <div class="col-sm-12"><?php $this->load->view('components/_mainmenu'); ?></div>
