0d05023e5b6db2e1317de30d2acf212223756d5d
marwansaleh <marwansaleh@192.168.1.14>
2015-11-19 17:38:26 +0700

    Change newlist index style

--- engine/application/views/frontend/newsindex/index.php
+++ engine/application/views/frontend/newsindex/index.php
@@ -44,7 +44,10 @@
                 <a href="<?php echo site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a>
             </h4>
             <span class="text-muted small"><em><?php echo date('D, d M Y',$article->date); ?></em></span>
-            <p><?php echo $article->synopsis; ?></p>
+            <p>
+                <?php echo $article->synopsis; ?> 
+                <a href="<?php echo site_url('detail/'.$article->url_title); ?>">...more</a>
+            </p>
         </li>
         <?php endforeach; ?>
     </ul>
