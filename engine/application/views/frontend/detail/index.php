0f36e18413f957fbc39a83d428ec7cec1c09b417
marwansaleh <marwansaleh@192.168.1.14>
2015-11-19 08:50:33 +0700

    Add author name in the bottom of news content

--- engine/application/views/frontend/detail/index.php
+++ engine/application/views/frontend/detail/index.php
@@ -16,6 +16,7 @@
                     <a href="#"><?php echo $article->created_by_name; ?></a>
                 </div>
                 <?php echo $article->content; ?>
+                <p class="text-muted small"><em>---Ditulis oleh <?php echo $article->created_by_name; ?></em></p>
                 <?php if ($article->tags): ?>
                 <div class="tag-container">
                     <div class="tag-title">Tags: </div>
