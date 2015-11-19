17886d916be945255ff4fa31ecd04fe1ecb43e55
marwansaleh <marwansaleh@192.168.1.14>
2015-11-19 14:54:05 +0700

    Add div root fb

--- engine/application/views/frontend/widgets/feature_video.php
+++ engine/application/views/frontend/widgets/feature_video.php
@@ -2,7 +2,8 @@
     <div class="inner">
         <h3>Featured video</h3>
         <div class="featured-video">
-            <iframe width="100%" height="195.75" src="http://www.youtube.com/embed/nKIu9yen5nc" frameborder="0" allowfullscreen=""></iframe>
+            <img src="http://placehold.it/348x196?text=Video">
+            <!--<iframe width="100%" height="195.75" src="http://www.youtube.com/embed/nKIu9yen5nc" frameborder="0" allowfullscreen=""></iframe>-->
         </div>
     </div>
 </div>
