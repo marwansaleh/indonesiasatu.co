266d941242ba3c0f702fba9c78d25db1c9682d3b
marwansaleh <marwansaleh@192.168.0.116>
2015-11-20 13:35:56 +0700

    Change newlist index style

--- engine/application/views/components/_mainmenu.php
+++ engine/application/views/components/_mainmenu.php
@@ -1,6 +1,8 @@
     <nav class="navbar navbar-default">
     <ul class="nav nav-justified">
-        <li <?php echo $active_menu=='home'?'class="active"':''; ?>><a href="<?php echo site_url(); ?>">Home</a></li>
+        <li <?php echo $active_menu=='home'?'class="active"':''; ?>><a href="<?php echo site_url(); ?>">
+                <img src="<?php echo site_url('assets/img/one.png'); ?>" > Home
+            </a></li>
         <?php if (isset($mainmenus)): ?>
         <?php foreach ($mainmenus as $menu): ?>
         <li <?php echo $active_menu==$menu->slug?'class="active"':''; ?>><a href="<?php echo site_url('category/'.$menu->slug); ?>"><?php echo $menu->name; ?></a></li>
