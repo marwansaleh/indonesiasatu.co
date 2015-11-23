6814cc6eeca07a0f8d136e196d24ed3ac8d1ae1b
marwansaleh <marwansaleh@192.168.0.116>
2015-11-23 21:43:37 +0700

    New widget inspirasi

--- engine/application/views/components/_side_right.php
+++ engine/application/views/components/_side_right.php
@@ -1,7 +1,5 @@
-<aside class="col-sm-4">
-    <div class="row">
-        <?php foreach ($widgets as $widget): ?>
-        <?php if (widget_exists($widget)): $this->load->view('frontend/widgets/' . $widget); endif; ?>
-        <?php endforeach; ?>
-    </div>
-</aside>
+<div class="row">
+    <?php foreach ($widgets as $widget): ?>
+    <?php if (widget_exists($widget)): $this->load->view('frontend/widgets/' . $widget); endif; ?>
+    <?php endforeach; ?>
+</div>
