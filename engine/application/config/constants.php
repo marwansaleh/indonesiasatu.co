78a881e1fb98789fa3e8003a2efc1a07e7105c71
marwansaleh <marwansaleh@192.168.0.116>
2015-11-23 22:38:31 +0700

    New widget inspirasi

--- engine/application/config/constants.php
+++ engine/application/config/constants.php
@@ -45,6 +45,7 @@ define('IMAGE_TYPE_MULTI','multi');
 
 define('IMAGE_THUMB_ORI', 'ori');
 define('IMAGE_THUMB_LARGE', 'large');
+define('IMAGE_THUMB_PORTRAIT', 'portrait');
 define('IMAGE_THUMB_MEDIUM', 'medium');
 define('IMAGE_THUMB_SMALL', 'small');
 define('IMAGE_THUMB_SQUARE', 'square');
