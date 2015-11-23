6ea0c89cb1d958ec32f30d853beb51f6328c1e96
marwansaleh <marwansaleh@192.168.0.116>
2015-11-23 21:42:00 +0700

    New widget inspirasi

--- engine/application/views/_layout_main.php
+++ engine/application/views/_layout_main.php
@@ -1,4 +1,8 @@
 <?php
 $this->load->view('components/_header_html');
-$this->load->view('components/_body_html');
+if (isset($active_menu) && $active_menu == 'home'){
+    $this->load->view('components/_body_html_home');
+}else{
+    $this->load->view('components/_body_html');
+}
 $this->load->view('components/_footer_html');
