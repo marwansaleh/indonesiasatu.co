4f620e42a5ffcd27c76e4f324dc74ed261e64edc
marwansaleh <marwansaleh@192.168.0.116>
2015-11-24 00:28:18 +0700

    new data model

---
+++ engine/application/models/article/attributes_m.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Tags_m
+ *
+ * @author Marwan
+ * @email amazzura.biz@gmail.com
+ */
+class Attributes_m extends MY_Model {
+    protected $_table_name = 'article_exts';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'category_id,name';
+    
+    public function save($data, $id = NULL) {
+        if ($id && $this->get_count(array('id!='=>$id,'category_id'=>$data['category_id'], 'att_name'=>$data['att_name']))){
+            $this->_last_message = 'Duplicate entry for '.$data['att_name'].' in category_id:'.$data['category_id'];
+            return FALSE;
+        }else if (!$id && $this->get_count(array('category_id'=>$data['category_id'], 'att_name'=>$data['att_name']))){
+            $this->_last_message = 'Duplicate entry for '.$data['att_name'].' in category_id:'.$data['category_id'];
+            return FALSE;
+        }
+        return parent::save($data, $id);
+    }
+}
+
+/*
+ * file location: /application/models/article/tags_m.php
+ */
