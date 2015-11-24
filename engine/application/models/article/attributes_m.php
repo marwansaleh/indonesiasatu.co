f7d79d1ce82699f3d03965fbf9f548ab547b10f9
marwansaleh <marwansaleh@192.168.1.13>
2015-11-24 16:23:34 +0700

    Add new controller ext_attrib

--- engine/application/models/article/attributes_m.php
+++ engine/application/models/article/attributes_m.php
@@ -12,6 +12,29 @@ class Attributes_m extends MY_Model {
     protected $_primary_filter = 'intval';
     protected $_order_by = 'category_id,attr_name';
     
+    public $rules = array(
+        'attr_name' => array(
+            'field' => 'attr_name',
+            'label' => 'Attribute name', 
+            'rules' => 'required|trim|xss_clean'
+        ),
+        'attr_label' => array(
+            'field' => 'slug',
+            'label' => 'Attribute label', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'attr_type' => array(
+            'field' => 'attr_type',
+            'label' => 'Attribute type', 
+            'rules' => 'trim|xss_clean'
+        ),
+        'category_id' => array(
+            'field' => 'category_id',
+            'label' => 'Category', 
+            'rules' => 'required|numeric|trim|xss_clean'
+        )
+    );
+    
     public function save($data, $id = NULL) {
         if ($id && $this->get_count(array('id!='=>$id,'category_id'=>$data['category_id'], 'attr_name'=>$data['attr_name']))){
             $this->_last_message = 'Duplicate entry for '.$data['attr_name'].' in category_id:'.$data['category_id'];
