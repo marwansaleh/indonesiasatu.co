c5b1eeeaa675c8320aba584c9af1fe33546dbb3a
marwansaleh <marwansaleh@192.168.1.13>
2015-11-24 20:43:54 +0700

    Add new controller ext_attrib

--- engine/application/views/components/_admin_menu.php
+++ engine/application/views/components/_admin_menu.php
@@ -7,11 +7,11 @@
     <li <?php echo $active_menu=='category'?'class="active"':''; ?>><a href="<?php echo site_url('cms/category'); ?>"><i class="fa fa-briefcase"></i> <span>Category</span></a></li>
     <li <?php echo $active_menu=='comment'?'class="active"':''; ?>><a href="#"><i class="fa fa-comments"></i> <span>Comments</span></a></li>
     <li <?php echo $active_menu=='newsticker'?'class="active"':''; ?>><a href="<?php echo site_url('cms/newsticker'); ?>"><i class="fa fa-link"></i> <span>Newsticker</span></a></li>
-    <li class="treeview">
+    <li class="treeview  <?php echo $active_menu=='advert'?'active':''; ?>">
         <a href="#"><i class="fa fa-adn"></i> <span>Adverts</span>  <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
-            <li><a href="#">Ads list</a></li>
-            <li><a href="#">Ads configuration</a></li>
+            <li><a href="<?php echo site_url('cms/advert'); ?>">Ads list</a></li>
+            <li><a href="<?php echo site_url('cms/advert_type'); ?>">Ads configuration</a></li>
         </ul>
     </li>
     <li class="treeview">
@@ -30,7 +30,7 @@
             <li><a href="<?php echo site_url('cms/userroles'); ?>">Access roles</a></li>
         </ul>
     </li>
-    <li class="treeview">
+    <li class="treeview  <?php echo $active_menu=='system'?'active':''; ?>">
         <a href="#"><i class="fa fa-cogs"></i> <span>Configuration</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
             <li><a href="<?php echo site_url('cms/sysconf'); ?>">System configuration</a></li>
