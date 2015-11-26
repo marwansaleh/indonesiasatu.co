dcb22a89fea7fc1822d1d97b94d4b1348a1cb72a
marwansaleh <marwansaleh@192.168.0.109>
2015-11-26 12:39:14 +0700

    Add new controller advert type

--- engine/application/models/weather/ow_city_m.php
+++ engine/application/models/weather/ow_city_m.php
@@ -10,7 +10,7 @@ class Ow_city_m extends MY_Model {
     protected $_table_name = 'owm_city';
     protected $_primary_key = 'id';
     protected $_primary_filter = 'intval';
-    protected $_order_by = 'country, name';
+    protected $_order_by = 'last_checked_time desc';
 }
 
 /*
