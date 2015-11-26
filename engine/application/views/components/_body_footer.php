de26dd5ab5fcbdce0275bbde1dd8354062a8f59e
marwansaleh <marwansaleh@192.168.0.109>
2015-11-26 10:31:32 +0700

    Add new controller advert type

--- engine/application/views/components/_body_footer.php
+++ engine/application/views/components/_body_footer.php
@@ -1,58 +1,69 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
-                <h3>Redaksi</h3>
-                <table class="table table-condensed redaksi small">
-                    <tr>
-                        <td style="width: 130px;">Pendiri/Pemimpin Umum</td>
-                        <td>Valens Daki-Soo</td>
-                    </tr>
-                    <tr>
-                        <td>Pemimpin Redaksi</td>
-                        <td>Simon Leya</td>
-                    </tr>
-                    <tr>
-                        <td>Dewan Pakar</td>
-                        <td>Letjen TNI (Purn) Kiki Syahnakri, K.H. Maman Imanulhaq, Dr. Yan Riberu, Ansel da Lopez, Dr.Ignas Iryanto Djou, Dr. Hanna R. Simatupang</td>
-                    </tr>
-                    <tr>
-                        <td>Iklan</td>
-                        <td>Wayan Pradnya Paramitha</td>
-                    </tr>
-                    <tr>
-                        <td>Konsultan Hukum</td>
-                        <td>Law Firm "VDS &amp; Partner"</td>
-                    </tr>
-                    <tr>
-                        <td>Penerbit</td>
-                        <td>Divisi Publishing PT. VERITAS DHARMA SATYA</td>
-                    </tr>
-                </table>
-                <p>IndoONEsia1@20015</p>
-            </div>
-            <div class="col-sm-4">
+            <div class="col-sm-3">
                 <img src="<?php echo site_url('assets/img/logo-putih.png'); ?>" class="img-responsive center-block" />
                 
-                <div class="col-sm-12 alamat">
-                    <div class="text-center">
-                        <h5><span class="text-bold">Alamat Redaksi</span></h5>
-                        <p>Gedung ITC Roxy Mas Blok D3 No.33 <br>Jl. K.H. Hasyim Ashari No.125 Gambir<br>Jakarta Pusat<br>Kontak Iklan<br>redaksi@indonesiasatu.co</p>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="redaksi-container">
+                            <p class="redaksi">
+                                Advisor: Letjen TNI (Purn) Kiki Syahnakri<br>
+                                Pendiri/Pemimpin Umum: Valens Daki-Soo<br>
+                                Pemimpin Redaksi: Simon Leya<br>
+                                Penerbit: PT Veritas Dharma Satya 
+                            </p>
+                            <p class="alamat">
+                                <span class="text-bold alamat-judul">Alamat Redaksi:</span><br>
+                                Jl. Serut No.06 RT.06, RW.04, Kel.<br>
+                                Kayu Putih, Kec. Pulogadung, Jaktim<br>
+                                Telp/Fax:021 - 4756205<br>
+                                Email: redaksi.indonesiasatu@gmail.com
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4">
-                <h3>KANAL BERITA</h3>
+            <div class="col-sm-9 kanal-berita">
                 <div class="row">
-                    <?php foreach($categories_articles as $cat_articles): ?>
                     <div class="col-sm-4">
-                        <ul class="kanal-list">
-                            <?php foreach ($cat_articles as $catbottom): ?>
-                            <li><a href="<?php echo site_url('category/'.$catbottom->slug); ?>"><?php echo $catbottom->name; ?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h3>PROFIL</h3>
+                                <ul class="kanal-list">
+                                    <li><a href="#">Tentang kami</a></li>
+                                    <li><a href="#">Redaksi</a></li>
+                                    <li><a href="#">Info iklan</a></li>
+                                    <li><a href="#">Hubungi kami</a></li>
+                                    <li><a href="#">Karir</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6">
+                                <h3>INSPIRASI</h3>
+                                <ul class="kanal-list">
+                                    <li><a href="#">Tentang kami</a></li>
+                                    <li><a href="#">Redaksi</a></li>
+                                    <li><a href="#">Info iklan</a></li>
+                                    <li><a href="#">Hubungi kami</a></li>
+                                    <li><a href="#">Karir</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <?php endforeach; ?>
+                    <div class="col-sm-8">
+                        <h3>KANAL BERITA</h3>
+                        <div class="row">
+                            <?php foreach($categories_articles as $cat_articles): ?>
+                            <div class="col-sm-4">
+                                <ul class="kanal-list">
+                                    <?php foreach ($cat_articles as $catbottom): ?>
+                                    <li><a href="<?php echo site_url('category/'.$catbottom->slug); ?>"><?php echo $catbottom->name; ?></a></li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
