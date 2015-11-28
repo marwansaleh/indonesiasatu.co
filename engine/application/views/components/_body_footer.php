2559ad1294d13b67aed280278c65f670035d717c
admin <admin@STAB01>
2015-11-28 12:18:45 +0700

    update channels

--- engine/application/views/components/_body_footer.php
+++ engine/application/views/components/_body_footer.php
@@ -52,7 +52,7 @@
                     </div>
                     <div class="col-sm-9">
                         <h3>KANAL</h3>
-                        <div class="row">
+                        <div class="row small">
                             <?php $i=1; foreach ($channels as $channel): ?>
                             <div class="col-sm-3" <?php if ($i%5==0) echo 'style="clear:both;"'; ?>>
                                 <h5 style="margin-bottom: 0;"><a style="color: orange!important;" href="<?php echo site_url('category/'.$channel->slug); ?>"><?php echo $channel->name; ?></a></h5>
