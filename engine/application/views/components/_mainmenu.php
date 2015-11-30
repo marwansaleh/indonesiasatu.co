4c4e7f8f8046cbd501b91721dd72d8069229a38d
marwansaleh <marwansaleh@192.168.1.12>
2015-11-30 14:28:18 +0700

    Add mainjs

--- engine/application/views/components/_mainmenu.php
+++ engine/application/views/components/_mainmenu.php
@@ -1,13 +1,18 @@
 <nav class="navbar navbar-default">
-    <ul class="nav nav-justified">
+    <ul id="mainmenu" class="nav nav-justified">
         <li <?php echo $active_menu=='home'?'class="active"':''; ?>><a href="<?php echo site_url(); ?>">
                 <img src="<?php echo site_url('assets/img/one.png'); ?>" >
             </a></li>
         <?php if (isset($mainmenus)): ?>
         <?php foreach ($mainmenus as $menu): ?>
-        <li <?php echo $active_menu==$menu->slug?'class="active"':''; ?>><a href="<?php echo site_url('category/'.$menu->slug); ?>"><?php echo $menu->name; ?></a></li>
+            <li data-children="<?php echo json_encode($menu->children); ?>" <?php echo $active_menu==$menu->slug?'class="active"':''; ?>>
+                <a href="<?php echo site_url('category/'.$menu->slug); ?>"><?php echo $menu->name; ?></a>
+            </li>
         <?php endforeach; ?>
         <?php endif; ?>
         <li <?php echo $active_menu=='newsindex'?'class="active"':''; ?>><a href="<?php echo site_url('newsindex'); ?>">Indeks</a></li>
     </ul>
 </nav>
+<div id="submenu" class="submenu">
+    <ul></ul>
+</div>
