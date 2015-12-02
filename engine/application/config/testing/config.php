44d23b6b8d8fa2126b3c6992271b2aa97f3243a0
admin <admin@STAB01>
2015-12-02 17:41:43 +0700

    update channels

--- engine/application/config/testing/config.php
+++ engine/application/config/testing/config.php
@@ -245,8 +245,8 @@ $config['encryption_key'] = '$#^ghafs@#';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expiration']		= 8640;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
@@ -266,7 +266,7 @@ $config['sess_time_to_update']  = 300;
 |
 */
 $config['cookie_prefix']	= "nsc_";
-$config['cookie_domain']	= "";
+$config['cookie_domain']	= "pre.indonesiasatu.co";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
