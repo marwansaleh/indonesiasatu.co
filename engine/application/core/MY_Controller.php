3c0b43f9424f7c97e9fe35807f7813d67f045bbf
admin <admin@STAB01>
2015-12-02 14:51:38 +0700

    update channels

--- engine/application/core/MY_Controller.php
+++ engine/application/core/MY_Controller.php
@@ -385,8 +385,8 @@ class MY_Controller extends MY_BaseController {
             'fb:app_id'         =>  $this->get_FB_ID(),
             'type'              => 'article',
             'local'             => 'id',
-            'article:author'    => 'IndonesiaSatu.co',
-            'article:publisher' => 'IndonesiaSatu.co'
+            //'article:author'    => 'IndonesiaSatu.co',
+            //'article:publisher' => 'IndonesiaSatu.co'
         );
     }
 }
