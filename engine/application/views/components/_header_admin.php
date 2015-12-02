a5a0e18d2f409a86e0030d26cd6187d7c6c3b23b
admin <admin@STAB01>
2015-12-02 16:17:35 +0700

    update channels

--- engine/application/views/components/_header_admin.php
+++ engine/application/views/components/_header_admin.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" type="image/gif" href="<?php echo site_url('favicon.png'); ?>"/>
+        <link rel="shortcut icon" type="image/gif" href="<?php echo site_url('assets/img/one.png'); ?>"/>
         <base href="<?php echo site_url(); ?>">
         <title><?php echo $meta_title; ?></title>
 
