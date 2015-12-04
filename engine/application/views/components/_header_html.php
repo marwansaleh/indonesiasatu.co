b287adc7841550e1a9cc07dbc6804f471affc882
marwansaleh <marwansaleh@192.168.1.15>
2015-12-04 13:31:47 +0700

    Add jscroll lib

--- engine/application/views/components/_header_html.php
+++ engine/application/views/components/_header_html.php
@@ -48,21 +48,4 @@
     </head>
     <body <?php echo isset($body_class) && $body_class?'class="'.$body_class.'"':''; ?>>
         <div id="fb-root"></div>
-        <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                  appId      : '<?php echo $FB_ID; ?>',
-                  xfbml      : true,
-                  version    : 'v2.5'
-                });
-            };
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.5";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-            
-        </script>
     
