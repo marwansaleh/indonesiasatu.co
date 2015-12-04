a1d244bd41661c9001ec31c6c6de56c90b3a0c7b
marwansaleh <marwansaleh@192.168.1.15>
2015-12-04 17:00:02 +0700

    Add jscroll lib

--- engine/application/views/frontend/widgets/latest_news.php
+++ engine/application/views/frontend/widgets/latest_news.php
@@ -8,7 +8,7 @@
                 <li>
                     <a title="" href="<?php echo site_url('detail/'.$latest->url_title); ?>">
                         <figure>
-                            <img src="<?php echo get_image_thumb($latest->image_url, IMAGE_THUMB_TINY); ?>" alt="">
+                            <img class="tiny" src="<?php echo get_image_thumb($latest->image_url, IMAGE_THUMB_TINY); ?>" alt="">
                         </figure>
                         <p><?php echo $latest->title; ?> <br> <span> <?php echo date('d M Y',$latest->date); ?> </span></p>
                     </a>
