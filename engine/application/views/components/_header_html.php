9f979373cccc8392c32edcfe83f170b1a58fb976
marwansaleh <marwansaleh@192.168.1.15>
2015-12-04 14:21:26 +0700

    Add jscroll lib

--- engine/application/views/components/_header_html.php
+++ engine/application/views/components/_header_html.php
@@ -45,6 +45,17 @@
           <script src="<?php echo site_url(config_item('path_lib').'respondjs/respond.min.js'); ?>"></script>
           <script src="<?php echo site_url(config_item('path_lib').'lte-ie7/lte-ie7.js'); ?>"></script>
         <![endif]-->
+        
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', '<?php echo $GA_Code; ?>', 'auto');
+          ga('send', 'pageview');
+
+        </script>
     </head>
     <body <?php echo isset($body_class) && $body_class?'class="'.$body_class.'"':''; ?>>
         <div id="fb-root"></div>
