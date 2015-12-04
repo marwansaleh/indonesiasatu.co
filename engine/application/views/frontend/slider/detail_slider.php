3bb315d3e17f7f6a6813492e5b410ee509fa1413
marwansaleh <marwansaleh@192.168.1.15>
2015-12-04 17:31:52 +0700

    Add jscroll lib

--- engine/application/views/frontend/slider/detail_slider.php
+++ engine/application/views/frontend/slider/detail_slider.php
@@ -10,7 +10,7 @@
     <div class="carousel-inner" role="listbox" style="max-height:450px;overflow:hidden;">
         <?php $i=0; foreach ($images as $image): ?>
         <div class="item <?php echo $i==0?'active':''; ?>">
-            <img src="<?php echo get_image_thumb($image->image_url, IMAGE_THUMB_LARGE); ?>">
+            <img class="large" src="<?php echo get_image_thumb($image->image_url, IMAGE_THUMB_LARGE); ?>">
         </div>
         <?php $i++; endforeach; ?>
     </div>
