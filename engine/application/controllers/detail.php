c23ec9cc313b8225276961f07da5c0c69d9765f3
marwansaleh <marwansaleh@192.168.1.15>
2015-12-04 15:50:21 +0700

    Add jscroll lib

--- engine/application/controllers/detail.php
+++ engine/application/controllers/detail.php
@@ -82,8 +82,7 @@ class Detail extends MY_News {
         if (in_array(WIDGET_SELECTED_CATEGORY, $widgets)){
             $this->data['selected_news_category'] = array(
                 'category'  => $selected_category,
-                'articles' => $this->_article_categories($selected_category->id, 
-                    isset($parameters['LAYOUT_HOME_CAT_ARTICLE_NUM'])?$parameters['LAYOUT_HOME_CAT_ARTICLE_NUM']:3)
+                'articles' => $this->_article_categories($selected_category->id, 5)
             );
         }
         
