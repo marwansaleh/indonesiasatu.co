8f5e1adf0a8955975b7f686a78753088b8d52fe2
marwansaleh <marwansaleh@192.168.0.107>
2015-12-07 17:51:29 +0700

    Add new service comment

--- engine/application/views/components/_footer_html.php
+++ engine/application/views/components/_footer_html.php
@@ -6,6 +6,14 @@
                   version    : 'v2.5'
                 });
             };
+            
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.5";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
         </script>
         <!-- bootstrap -->
         <script src="<?php echo site_url(config_item('path_lib').'bootstrap/js/bootstrap.min.js'); ?>"></script>
