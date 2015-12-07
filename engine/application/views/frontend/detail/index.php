852f06cebe9a53f5c2d5e5d3836ce43c79f13788
marwansaleh <marwansaleh@192.168.0.107>
2015-12-07 17:50:15 +0700

    Add new service comment

--- engine/application/views/frontend/detail/index.php
+++ engine/application/views/frontend/detail/index.php
@@ -211,33 +211,4 @@
             }
         }
     };
-    
-    window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '<?php echo $FB_ID; ?>',
-          xfbml      : true,
-          version    : 'v2.5'
-        });
-    };
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.5";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    function share_fb(link){
-        
-        FB.ui({
-          method: 'share',
-          href: link
-        }, function(response){}); 
-        
-        return false;
-    }
-    function share_tw(encoded_url,text){
-        var tw_window = window.open('https://twitter.com/intent/tweet?url='+encoded_url+'&text='+text,'Twitter-Web-Intent');
-        tw_window.focus();
-    }
 </script>
