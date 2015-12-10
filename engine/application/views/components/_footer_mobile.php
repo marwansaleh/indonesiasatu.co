187137a4330d2137aa4a765867e3dece735a5cf7
marwansaleh <marwansaleh@192.168.1.16>
2015-12-10 10:06:04 +0700

    FB setup

--- engine/application/views/components/_footer_mobile.php
+++ engine/application/views/components/_footer_mobile.php
@@ -1,4 +1,12 @@
-        <script type="text/javascript">
+    <footer>
+        <p class="redaksi">
+            Gedung ITC Roxy Mas Blok D3 No. 33
+            Jl. Kh. Hasyim Ashari No. 125, Gambir, Jakarta Pusat,
+            Telp/Fax:021 - 4756205, Email: redaksi@indonesiasatu.co, 
+        </p>
+        <p class="copyright">Copyright@2015IndonesiaSatu.co</p>
+    </footer>
+    <script type="text/javascript">
             $(document).ready(function(){
                 $('.media-list').on('click','.media',function(){
                     if ($(this).attr('data-href')){
