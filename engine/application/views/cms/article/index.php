52f4c5e8f667bd5083a8163ab558ef2218a8e239
marwansaleh <marwansaleh@192.168.1.101>
2015-12-13 23:53:41 +0700

    FB setup

--- engine/application/views/cms/article/index.php
+++ engine/application/views/cms/article/index.php
@@ -79,7 +79,7 @@
 </div>
 
 <div class="modal fade" tabindex="-1" role="dialog" id="MyModal">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
