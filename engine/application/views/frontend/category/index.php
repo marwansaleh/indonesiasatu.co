fd83bb88b364460546a6295fa9597b2c5bc2a305
marwansaleh <marwansaleh@192.168.1.13>
2015-12-14 11:51:19 +0700

    FB setup

--- engine/application/views/frontend/category/index.php
+++ engine/application/views/frontend/category/index.php
@@ -4,13 +4,13 @@
     <div class="blog-page">
         <article>
             <h1 class="title">
-                <a href="<?php echo site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a>
+                <a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a>
             </h1>
             <?php if ($article->image_type==IMAGE_TYPE_MULTI): ?>
             <?php $this->load->view('frontend/slider/detail_slider', array('images'=>$article->images)); ?>
             <?php else: ?>
             <figure>
-                <a href="<?php echo site_url('detail/'.$article->url_title); ?>">
+                <a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>">
                     <img class="img-responsive large" src="<?php echo get_image_thumb($article->image_url, IMAGE_THUMB_LARGE); ?>" alt="Article image">
                 </a>
             </figure>
@@ -34,11 +34,11 @@
                 </figure>
                 <div class="text">
                     <div class="inner-border">
-                        <div class="title"><a href="<?php echo site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a></div>
+                        <div class="title"><a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a></div>
                         <div class="description">
                             <div class="date"><?php echo number_format($article->comment); ?> comments, <?php echo date('d/m/Y',$article->date); ?>, by <?php echo $article->created_by_name; ?></div>
                             <div class="excerpt">
-                                <p><?php echo $article->synopsis; ?> <a href="<?php echo site_url('detail/'.$article->url_title); ?>">Read more...</a></p>
+                                <p><?php echo $article->synopsis; ?> <a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>">Read more...</a></p>
                             </div>
                         </div>
                     </div>
