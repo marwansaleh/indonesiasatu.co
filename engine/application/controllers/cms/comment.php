6f67947a1a699b9c8f7b7f1033a6f2999ab3a65d
marwansaleh <marwansaleh@192.168.1.29>
2015-12-16 15:14:02 +0700

    FB setup

--- engine/application/controllers/cms/comment.php
+++ engine/application/controllers/cms/comment.php
@@ -16,8 +16,9 @@ class Comment extends MY_AdminController {
         $this->load->model(array('article/article_m'));
     }
     
-    function index(){
-        
+    function index($article_id=0){
+        $this->data['articles'] = $this->article_m->get_select_where('id,title',array('comment >'=>0));
+        $this->data['selected_article_id'] = $article_id;
         
         //set breadcumb
         breadcumb_add($this->data['breadcumb'], 'Comments', site_url('cms/comment'), TRUE);
