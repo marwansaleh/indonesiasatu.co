4c17c24748b1b57c12e1158e5a4c63865583ca6e
marwansaleh <marwansaleh@192.168.1.102>
2016-02-29 03:12:05 +0700

    Update migration jenis_asuransi --> feeding data

--- engine/application/views/frontend/home/index.php
+++ engine/application/views/frontend/home/index.php
@@ -71,24 +71,8 @@
             <?php if ($category_refleksi->articles): ?>
             <div class="articles-slider">
                 <div class="flex-viewport">
-                    <?php $i=1; foreach ($category_refleksi->articles as $article): ?>
+                    <?php foreach ($category_refleksi->articles as $article): ?>
 
-                    <?php if ($i==1): ?>
-                    <div class="main-article">
-                        <div class="title">
-                            <span><a href="<?php echo $article->url_short ? $article->url_short : site_url('detail/'.$article->url_title); ?>"><?php echo $article->title; ?></a></span>
-                        </div>
-                        <figure>
-                            <img class="medium" src="<?php echo get_image_thumb($article->image_url, IMAGE_THUMB_MEDIUM); ?>" alt="">
-                        </figure>
-                        <div class="main-text">
-                            <div class="inner">
-                                <span class="article-info"><?php echo number_format($article->comment); ?> comments, <?php echo date('d/m/Y',$article->date); ?></span>
-                                <p><?php echo $article->synopsis; ?> <a href="<?php echo site_url('detail/'.$article->url_title); ?>">Read more...</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <?php else: ?>
                     <article>
                         <figure style="overflow:hidden;"><img class="img-responsive" src="<?php echo get_image_thumb($article->image_url, IMAGE_THUMB_SMALL); ?>" alt=""></figure>
                         <div class="text">
@@ -96,8 +80,7 @@
                             <span class="info"><?php echo date('d/m/Y',$article->date); ?>, <?php echo number_format($article->comment); ?> comments</span>
                         </div>
                     </article>
-                    <?php endif; ?>
-                    <?php $i++; endforeach; ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
             <?php endif; ?>
