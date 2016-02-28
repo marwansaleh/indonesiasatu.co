8bba0c569892f99dc114c29f3dd92aa8fa6f1679
marwansaleh <marwansaleh@192.168.1.102>
2016-02-29 03:43:40 +0700

    Update migration jenis_asuransi --> feeding data

--- engine/application/views/components/_side_right.php
+++ engine/application/views/components/_side_right.php
@@ -3,3 +3,6 @@
     <?php if (widget_exists($widget)): $this->load->view('frontend/widgets/' . $widget); endif; ?>
     <?php endforeach; ?>
 </div>
+<?php if (isset($iklan_gabung)): ?>
+<div class="row"><img src="<?php echo $iklan_gabung; ?>" class="img-responsive" style="margin-top: 5px;" /></div>
+<?php endif; ?>
