d2a3bf1fe6c3a904bbf67ececbeb1d36374cbdba
marwansaleh <marwansaleh@192.168.1.101>
2016-05-29 13:38:35 +0700

    Update Closing and Pembukuan

--- engine/application/views/components/_footer_mobile.php
+++ engine/application/views/components/_footer_mobile.php
@@ -1,9 +1,23 @@
     <footer>
-        <p class="redaksi">
+        <div id="back-top" class="row">
+            <div class="col-xs-12 text-center">
+                <a href="#top">Kembali ke atas</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <ul id="main-bottom-menus">
+                    <?php $i=0; foreach ($mainmenus as $mainmenu): ?>
+                    <li><a href="<?php echo site_url('category/'.$mainmenu->slug); ?>"><?php echo $mainmenu->name; ?></a></li>
+                    <?php $i++; endforeach; ?>
+                </ul>
+            </div>
+        </div>
+<!--        <p class="redaksi">
             Gedung ITC Roxy Mas Blok D3 No. 33
             Jl. Kh. Hasyim Ashari No. 125, Gambir, Jakarta Pusat,
             Telp/Fax:021-4756205, Email: <a href="mailto:redaksi@indonesiasatu.co">redaksi@indonesiasatu.co</a>, 
-        </p>
+        </p>-->
         <p class="copyright">Copyright@2015IndonesiaSatu.co</p>
     </footer>
     <script type="text/javascript">
