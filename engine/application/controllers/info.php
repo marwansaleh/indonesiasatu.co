fc16c08870961e8563a95b456e407abd4ab2c278
marwansaleh <marwansaleh@172.18.1.216>
2016-05-30 12:01:24 +0700

    Update Klaim Models

--- engine/application/controllers/info.php
+++ engine/application/controllers/info.php
@@ -8,6 +8,13 @@ class Info extends MY_Controller{
     function phpbindir(){
         echo "PHPBINDIR: ". PHP_BINDIR;
     }
+    
+    function log($filename){
+        $log_file = rtrim(sys_get_temp_dir(),'/') .'/' . $filename;
+        $log = $this->read_log(10,$log_file);
+        
+        echo '<pre>'.$log.'</pre>';
+    }
 }
 /* 
  * To change this license header, choose License Headers in Project Properties.
