5371b60c40e362d1599cc3cb80ec21fec04fd065
marwansaleh <marwansaleh@172.18.1.216>
2016-06-05 11:46:50 +0700

    Update migration sales

--- engine/application/views/components/_footer_mobile.php
+++ engine/application/views/components/_footer_mobile.php
@@ -14,7 +14,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xs-offset-3 col-xs-6">
+            <div class="col-xs-offset-3 col-xs-6" style="border-top: solid 1px #CCC; border-bottom: solid 1px #CCC;">
                 <ul id="helper-bottom-menus">
                     <li>
                         <a href="<?php echo site_url('staticpage/index/redaksi'); ?>">Redaksi</a>
