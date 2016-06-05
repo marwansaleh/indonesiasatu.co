e82c7392c0b2b4d49bf3c5e7ce6c2cb09a099192
marwansaleh <marwansaleh@172.18.1.216>
2016-06-05 12:14:01 +0700

    Update migration sales

--- engine/application/views/mobile/staticpage/index.php
+++ engine/application/views/mobile/staticpage/index.php
@@ -1,4 +1,6 @@
-<div class="main">
-    <h1 class="title"><?php echo $staticpage->title; ?></h1>
-    <?php echo $staticpage->content; ?>
+<div class="blog-page">
+    <article>
+        <h1 class="title"><?php echo $staticpage->title; ?></h1>
+        <?php echo $staticpage->content; ?>
+    </article>
 </div>
