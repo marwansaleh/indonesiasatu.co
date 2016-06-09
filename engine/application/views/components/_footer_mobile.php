f40a1343c7b850bc87affd422468ff7b20570aa6
marwansaleh <marwansaleh@172.18.1.216>
2016-06-09 07:56:57 +0700

    Update migration sales

--- engine/application/views/components/_footer_mobile.php
+++ engine/application/views/components/_footer_mobile.php
@@ -41,7 +41,7 @@
                 <a href="http://www.veritasdharmasatya.com" target="_blank"><span>www.veritasdharmasatya.com</span></a>
             </div>
         </div>
-        <p class="redaksi">
+        <p class="redaksi" style="font-size: 8px;">
             Gedung ITC Roxy Mas Blok D3 No. 33
             Jl. Kh. Hasyim Ashari No. 125, Gambir, Jakarta Pusat,
             Telp/Fax:021-4756205, Email: <a href="mailto:redaksi@indonesiasatu.co">redaksi@indonesiasatu.co</a>, 
