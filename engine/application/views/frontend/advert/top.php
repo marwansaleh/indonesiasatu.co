85c290f97e37ef05c295bae0d1aaae9d5d8b8bfe
marwansaleh <marwansaleh@192.168.1.103>
2016-07-01 23:42:23 +0700

    Advertisment

--- engine/application/views/frontend/advert/top.php
+++ engine/application/views/frontend/advert/top.php
@@ -3,7 +3,7 @@
     <?php foreach ($adverts[ADV_TYPE_TOP] as $adv): ?>
     <div class="col-sm-12">
         <?php if ($adv->link_url && $adv->link_url != '#'): ?>
-        <a href="<?php echo $adv->link_url; ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
+        <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
             <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
         </a>
         <?php else: ?>
