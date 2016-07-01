f741f5932959a4c45646d378e99f8bf7f8fb42b0
marwansaleh <marwansaleh@192.168.1.103>
2016-07-01 23:02:16 +0700

    Advertisment

--- engine/application/controllers/cms/advert.php
+++ engine/application/controllers/cms/advert.php
@@ -12,7 +12,7 @@ class Advert extends MY_AdminController {
         $this->data['page_description'] = 'List and update adverts';
         
         //load models
-        $this->load->model(array('advert/advtype_m','advert/advert_m'));
+        $this->load->model(array('advert/advert_m'));
     }
     
     function index(){
@@ -33,7 +33,7 @@ class Advert extends MY_AdminController {
             $items = $this->advert_m->get_offset('*',$where,$offset,  $this->REC_PER_PAGE);
             if ($items){
                 foreach($items as $item){
-                    $item->advert_type = $this->advtype_m->get_value('name',array('id'=>$item->type));
+                    $item->advert_type = advert_type($item->type);
                     $this->data['items'][] = $item;
                 }
                 $url_format = site_url('cms/advert/index?page=%i');
@@ -65,89 +65,26 @@ class Advert extends MY_AdminController {
         $this->data['item'] = $item;
         
         //suporting data
-        $this->data['advert_types'] = $this->advtype_m->get();
+        $this->data['advert_types'] = advert_type();
         
         //set breadcumb
         breadcumb_add($this->data['breadcumb'], 'Adverts', site_url('cms/advert'));
         breadcumb_add($this->data['breadcumb'], 'Update Item', NULL, TRUE);
         
-        $this->data['submit_url'] = site_url('cms/advert/save?id='.$id.'&page='.$page);
         $this->data['back_url'] = site_url('cms/advert/index?page='.$page);
         $this->data['subview'] = 'cms/advert/edit';
         $this->load->view('_layout_admin', $this->data);
-    }
-    
-    function save(){
-        $id = $this->input->get('id', TRUE);
-        $page = $this->input->get('page', TRUE);
-        
-//        if ((!$id && !$this->users->has_access('CATEGORY_CREATE'))||($id && !$this->users->has_access('CATEGORY_EDIT'))){
-//            $this->session->set_flashdata('message_type','error');
-//            $this->session->set_flashdata('message', 'Sorry. You dont have access for this feature');
-//            redirect('cms/category/index?page='.$page);
-//        }
-        
-        $rules = $this->advert_m->rules;
-        $this->form_validation->set_rules($rules);
-        //exit(print_r($rules));
-        if ($this->form_validation->run() != FALSE) {
-            $postdata = $this->advert_m->array_from_post(array('name','type','link_url','new_window','start_date','end_date'));
-            
-            if ($this->advert_m->save($postdata, $id)){
-                $this->session->set_flashdata('message_type','success');
-                $this->session->set_flashdata('message', 'Data item saved successfully');
-                
-                redirect('cms/advert/index?page='.$page);
-            }else{
-                $this->session->set_flashdata('message_type','error');
-                $this->session->set_flashdata('message', $this->advert_m->get_last_message());
-            }
-        }
-        
-        if (validation_errors()){
-            $this->session->set_flashdata('message_type','error');
-            $this->session->set_flashdata('message', validation_errors());
-        }
-        
-        redirect('cms/advert/edit?id='.$id.'&page='.$page);
-    }
-    
-    function copy(){
-        $id = $this->input->get('id', TRUE);
-        $page = $this->input->get('page', TRUE);
-        
-        $item = $this->advert_m->get($id);
-        if (!$item){
-            $this->session->set_flashdata('message_type','error');
-            $this->session->set_flashdata('message', 'Could not find data item. Copy item failed!');
-        }else{
-            $copy_data = array();
-            foreach ($item as $key => $value){
-                if ($key != 'id'){
-                    $copy_data[$key] = $value;
-                }
-            }
-            if ($this->advert_m->save($copy_data)){
-                $this->session->set_flashdata('message_type','success');
-                $this->session->set_flashdata('message', 'Data item copied successfully');
-            }else{
-                $this->session->set_flashdata('message_type','error');
-                $this->session->set_flashdata('message', $this->advert_m->get_last_message());
-            }
-        }
-        
-        redirect('cms/advert/index?page='.$page);
     }
     
     function delete(){
         $id = $this->input->get('id', TRUE);
         $page = $this->input->get('page', TRUE);
         
-//        if (!$this->users->has_access('CATEGORY_DELETE')){
-//            $this->session->set_flashdata('message_type','error');
-//            $this->session->set_flashdata('message', 'Sorry. You dont have access for this feature');
-//            redirect('cms/category/index?page='.$page);
-//        }
+        if (!$this->users->has_access('ADDS_MANAGEMENT')){
+            $this->session->set_flashdata('message_type','error');
+            $this->session->set_flashdata('message', 'Sorry. You dont have access for this feature');
+            redirect('cms/advert/index?page='.$page);
+        }
         
         //check if found data item
         $item = $this->advert_m->get($id);
@@ -156,6 +93,10 @@ class Advert extends MY_AdminController {
             $this->session->set_flashdata('message', 'Could not find data item. Delete item failed!');
         }else{
             if ($this->advert_m->delete($id)){
+                //delete the file
+                if (file_exists($item->file_name)){
+                    unlink($item->file_name);
+                }
                 $this->session->set_flashdata('message_type','success');
                 $this->session->set_flashdata('message', 'Data item deleted successfully');
             }else{
