715fc5ab06f42f2dc71284e9ddccb2f115f74139
marwansaleh <marwansaleh@192.168.1.111>
2016-08-17 10:25:48 +0700

    Update disposisi direksi approval

--- engine/application/views/frontend/widgets/facebook.php
+++ engine/application/views/frontend/widgets/facebook.php
@@ -1,8 +1,10 @@
 <div class="widget hidden-xs">
     <div class="inner">
         <h3>Follow us on Facebook</h3>
-        <div class="follow-us">
-            <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fplatform&amp;width=292&amp;height=258&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color=%23fff&amp;header=false&amp;appId=225987124123858" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:258px;" allowtransparency="true"></iframe>
+        <div class="fb-page" data-href="https://www.facebook.com/IndonesiaSatu.co" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/IndonesiaSatu.co" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/IndonesiaSatu.co">IndonesiaSatu.co</a>
+            </blockquote>
         </div>
     </div>
 </div>
