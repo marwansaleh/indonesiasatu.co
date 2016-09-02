af9bf49cfaa37435c01fb07ecb994419c6fe3388
marwansaleh <marwansaleh@192.168.1.101>
2016-09-03 00:47:59 +0700

    tidnaklanjut status

--- engine/application/views/frontend/advert/mobile_body.php
+++ engine/application/views/frontend/advert/mobile_body.php
@@ -1,5 +1,5 @@
 <?php foreach ($adverts[ADV_TYPE_MOBILE_BODY] as $adv): ?>
-<div class="col-sm-12">
+<div class="col-sm-12 mobile-adv-content">
     <?php if ($adv->link_url && $adv->link_url != '#'): ?>
     <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
         <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
