b8a9761b0164f0f0c0e6072351ac037fc42278b3
Marwan Saleh <marwan@bsminsbroker.com>
2016-12-07 10:46:48 +0700

    Add new model forgot_passwd_m

--- engine/application/controllers/service/user.php
+++ engine/application/controllers/service/user.php
@@ -194,7 +194,7 @@ class User extends REST_Api {
             }
             $headers  = 'MIME-Version: 1.0' . "\r\n";
             $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-            $headers .= 'From: IndonesiaSatu.co Administrator <marwan@indonesiasatu.co>' . "\r\n" .
+            $headers .= 'From: IndonesiaSatu.co Administrator <no-reply@indonesiasatu.co>' . "\r\n" .
                         'X-Mailer: PHP/' . phpversion();
             $send_email = mail($recipient, $subject, $content, $headers);
             if ($send_email){
@@ -253,7 +253,7 @@ class User extends REST_Api {
                 }
                 $headers  = 'MIME-Version: 1.0' . "\r\n";
                 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-                $headers .= 'From: IndonesiaSatu.co Administrator <marwan@indonesiasatu.co>' . "\r\n" .
+                $headers .= 'From: IndonesiaSatu.co Administrator <no-reply@indonesiasatu.co>' . "\r\n" .
                             'X-Mailer: PHP/' . phpversion();
                 $send_email = mail($recipient, $subject, $content, $headers);
             }
