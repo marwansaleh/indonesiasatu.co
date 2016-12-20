bccae9131ed719757604fbdb0cbb4684a846040f
Marwan Saleh <marwan@bsminsbroker.com>
2016-12-21 00:32:53 +0700

    Add migration master kancab bsm, kanwil bri pic, kancab bsm and kanwil bri relations

--- engine/application/views/frontend/advert/mobile_bottom.php
+++ engine/application/views/frontend/advert/mobile_bottom.php
@@ -1,11 +1,28 @@
-<?php foreach ($adverts[ADV_TYPE_MOBILE_BOTTOM] as $adv): ?>
-<div class="adv">
-    <?php if ($adv->link_url && $adv->link_url != '#'): ?>
-    <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
+<div id="mobile-bottom-advert">
+    <?php foreach ($adverts[ADV_TYPE_MOBILE_BOTTOM] as $adv): ?>
+    <div class="adv">
+        <?php if ($adv->link_url && $adv->link_url != '#'): ?>
+        <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
+            <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
+        </a>
+        <?php else: ?>
         <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
-    </a>
-    <?php else: ?>
-    <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
-    <?php endif; ?>
+        <?php endif; ?>
+    </div>
+    <?php endforeach; ?>
 </div>
-<?php endforeach; 
+
+<?php if (count($adverts[ADV_TYPE_MOBILE_BOTTOM])): ?>
+<script type="text/javascript">
+    $(document).ready(function (){
+        $('#mobile-bottom-advert').flexslider({
+            animation: "slide",
+            //slideshow: true,
+            controlNav: false,
+            animationLoop: true,
+            //itemWidth: 400,
+            itemMargin: 0
+        });
+    });
+</script>
+<?php endif; 
