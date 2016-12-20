d5c3901a0dc3f87ab2c90a7ecbd89bc7a4424cc6
Marwan Saleh <marwan@bsminsbroker.com>
2016-12-21 00:54:18 +0700

    Add migration master kancab bsm, kanwil bri pic, kancab bsm and kanwil bri relations

--- engine/application/views/frontend/advert/mobile_top.php
+++ engine/application/views/frontend/advert/mobile_top.php
@@ -1,4 +1,4 @@
-<div class="flexslider" id="mobile-top-advert">
+<div class="flexslider" id="mobile-top-advert" style="margin-bottom: 0;">
     <ul class="slides">
         <?php foreach ($adverts[ADV_TYPE_MOBILE_TOP] as $adv): ?>
         <li>
