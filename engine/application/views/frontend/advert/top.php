301477b54645042db5ef22d127237fbe07d02a3c
Marwan Saleh <marwan@bsminsbroker.com>
2016-12-21 08:05:17 +0700

    Advert type mobile article

--- engine/application/views/frontend/advert/top.php
+++ engine/application/views/frontend/advert/top.php
@@ -1,15 +1,33 @@
 <?php if ($adverts && isset($adverts[ADV_TYPE_TOP])): ?>
 <div class="row">
-    <?php foreach ($adverts[ADV_TYPE_TOP] as $adv): ?>
-    <div class="col-sm-12">
-        <?php if ($adv->link_url && $adv->link_url != '#'): ?>
-        <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
-            <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
-        </a>
-        <?php else: ?>
-        <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
-        <?php endif; ?>
+    <div class="col-sm-12 <?php echo count($adverts[ADV_TYPE_TOP])>1?'flexslider':''; ?>" id="advert-top-desktop">
+        <ul ul class="slides">
+            <?php foreach ($adverts[ADV_TYPE_TOP] as $adv): ?>
+            <li>
+                <?php if ($adv->link_url && $adv->link_url != '#'): ?>
+                <a href="<?php echo site_url('click/run/'.$adv->id.'/'.  urlencode(base64_encode($adv->link_url))); ?>" <?php echo $adv->new_window==1?'target="blank"':''; ?>>
+                    <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
+                </a>
+                <?php else: ?>
+                <img class="img-responsive" src="<?php echo $adv->file_name; ?>">
+                <?php endif; ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
-    <?php endforeach; ?>
 </div>
+<?php endif; ?>
+<?php if (count($adverts[ADV_TYPE_TOP])>1): ?>
+<script type="text/javascript">
+    $(document).ready(function (){
+        $('#advert-top-desktop').flexslider({
+            animation: "slide",
+            slideshow: true,
+            controlNav: false,
+            animationLoop: true,
+            //itemWidth: 400,
+            itemMargin: 0
+        });
+    });
+</script>
 <?php endif; 
