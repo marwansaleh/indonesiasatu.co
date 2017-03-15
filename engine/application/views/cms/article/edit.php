b055b06ba72b072153fa85cd6e68d6cf85b91f41
Marwan Saleh <marwan@bsminsbroker.com>
2017-03-15 17:13:45 +0700

    Update laporan

--- engine/application/views/cms/article/edit.php
+++ engine/application/views/cms/article/edit.php
@@ -354,7 +354,7 @@
         toolbar1: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect | responsivefilemanager | link unlink anchor | image media | forecolor backcolor  | print preview code ",
         //toolbar2: "",
         image_advtab: true,
-        external_filemanager_path:"/<?php echo config_item('path_lib').'filemanager'; ?>/",
+        external_filemanager_path:"<?php echo site_url(config_item('path_lib').'filemanager'); ?>/",
         filemanager_title:"Filemanager" ,
         filemanager_crossdomain: true,
         external_plugins: { "filemanager" : "<?php echo site_url(config_item('path_lib').'filemanager/plugin.min.js'); ?>"},
